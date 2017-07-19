bbe247dc3c63a0006d0dfca30321d109c6e46ff2
Vivian Dao (AIM CONSULTING GROUP) <v-vydao@microsoft.com>
2017-07-19 17:34:16 +0000

    Merged PR 84: Merge Viv to master
    Team Setup Screen

--- Jeopardy/TeamSetupViewController.swift
+++ Jeopardy/TeamSetupViewController.swift
@@ -8,8 +8,60 @@
 
 import UIKit
 
-class TeamSetupViewController: UIViewController {
+class TeamSetupViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
 
+    var names: [String] = []
+    var jeopardy = AppDelegate.Manager
+    
+    @IBOutlet weak var enterTeamName: UITextField!
+    @IBOutlet weak var jeopardyTable: UITableViewCell!
+    @IBOutlet weak var teamNames: UILabel!
+    @IBOutlet weak var teamTable: UITableView!
+    @IBOutlet weak var startGame: UIButton!
+    
+    @IBAction func addTeamName(_ sender: Any) {
+        Manager.AddNewTeam(enterTeamName?.text)
+            }
+    
+   
+    
+   
+    @IBAction func startButtonPressed(_ sender: Any) {
+    let destination = UIStoryboard(name: "GameScreenViewController", bundle: nil).instantiateInitialViewController() as! GameScreenViewController
+        destination.modalTransitionStyle = UIModalTransitionStyle.crossDissolve
+        self.present(destination, animated: true, completion: nil)
+
+    }
+ 
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.names.count
+    }
+    
+    func tableView(_ tableViewL: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = self.teamTable.dequeueReusableCell(withIdentifier:"teamNameCell", for: indexPath)
+    
+   return cell
+    
+    }
+ 
+    
+    override func viewWillAppear(_ animated: Bool) {
+        Manager.RegisterNewTeamAddedHandler( {teams in
+            self.names = teams
+            self.teamTable.reloadData()
+            if self.names.count >= 4 {
+                startGame.isEnabled = true
+            }
+            else {
+                startGame.isEnabled = false
+            }
+            
+
+        })
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,15 +73,5 @@ class TeamSetupViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
