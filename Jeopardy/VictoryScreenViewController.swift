e7a751df4ff070a235327cb64f90133d3ea2b135
Sulekha Ali <v-suleal@microsoft.com>
2017-07-20 10:41:21 -0700

    code cleanup

--- Jeopardy/VictoryScreenViewController.swift
+++ Jeopardy/VictoryScreenViewController.swift
@@ -19,37 +19,33 @@ class VictoryScreenViewController: UIViewController {
     @IBOutlet weak var thirdPlace: UILabel!
     
     @IBOutlet weak var fourthPlace: UILabel!
-//    
 
     var teamInfo:[Team] = []
     var sortedTeams:[Team] = []
 
     
     override func viewWillAppear(_ animated: Bool) {
+        // delete later!
+        createTestData()
+        // end delete!
+        
+        sortedTeams = teamInfo.sorted(by: { $0.score > $1.score })
         highScore()
         otherPlaces()
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
     func highScore(){
-//  array of teams structs
-//  take into account all scores, order highest to low in array
-//  array in order of highest to lowest
-//  var winningTeam = array[0]
-//  winnerAnnounced.text =   "You Rock,: \((winningTeam))"
-//    winnerScore.text = "You scored
-       
-        // delete later!
+        winnerAnnounced.text =   "The Winner is: \((sortedTeams[0].name))!"
+        winnerScore.text = "You Scored \((sortedTeams[0].score))"
+    }
+    
+    func otherPlaces(){
+        secondPlace.text = "Second place goes to: \((sortedTeams[1].name))"
+        thirdPlace.text = "Third place goes to: \((sortedTeams[2].name))"
+        fourthPlace.text = "Fourth place goes to: \((sortedTeams[3].name))"
+    }
+    
+    func createTestData() {
         var team1:Team = Team(name: "t1")
         team1.score = 150
         teamInfo.append(team1)
@@ -65,35 +61,5 @@ class VictoryScreenViewController: UIViewController {
         var team4:Team = Team(name: "t4")
         team4.score = 300
         teamInfo.append(team4)
-        // end delete!
-        
-        
-        
-        sortedTeams = teamInfo.sorted(by: { $0.score > $1.score })
-        winnerAnnounced.text =   "The Winner is: \((sortedTeams[0].name))!"
-        winnerScore.text = "You Scored \((sortedTeams[0].score))"
     }
-    
-    func otherPlaces(){
-        secondPlace.text = "Second place goes to: \((sortedTeams[1].name))"
-        
-        thirdPlace.text = "Third place goes to: \((sortedTeams[2].name))"
-        
-        fourthPlace.text = "Fourth place goes to: \((sortedTeams[3].name))"
-        
-    }
-    
-
-//    switch case?
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
